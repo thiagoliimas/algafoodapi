df3cfc4ec973abb352160b1f6fbf5a7d172beffd
ThiagoLima <thiagoliimas@outlook.com>
2023-09-05 20:27:40 -0300

    Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algafood/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algafood/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algafood.algafoodapi.di.notificacao;
 
 import com.algafood.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador{
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.port}")
+    private Integer port;
+
     public NotificadorEmail() {
         System.out.println("Notificador de email REAL");
     }
@@ -16,5 +23,6 @@ public class NotificadorEmail implements Notificador{
     public void notificar(Cliente cliente, String mensagem){
         System.out.printf("Notificando cliente %s através do email %s - Mensagem: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.println("Host: " + host + " - Porta: " + port);
     }
 }
