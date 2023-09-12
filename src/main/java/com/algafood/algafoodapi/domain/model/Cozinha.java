e1795980ca4c7b332f2cab8b40bbccf5d7febbea
ThiagoLima <thiagoliimas@outlook.com>
2023-09-12 00:22:05 -0300

    Criado model Cozinha e Restaurante

--- src/main/java/com/algafood/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algafood/algafoodapi/domain/model/Cozinha.java
@@ -1,4 +1,46 @@
 package com.algafood.algafoodapi.domain.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "cozinha")
 public class Cozinha {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
